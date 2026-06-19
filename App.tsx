85370a4602effe7d9f6f4c8920b3be78fa398f8e
patties-eater <anjugautam7223@gmail.com>
2026-06-19 16:06:31 +0545

    home page made and ui  and map  to be refined .

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import AuthScreen from './screens/AuthScreen';
+// import AuthScreen from './screens/AuthScreen';
 import { NotificationProvider } from './components/Notification';
+import HomeScreen from './screens/Home';
 
 export default function App() {
   return (
     <NotificationProvider>
       <StatusBar style="dark" />
-      <AuthScreen />
+     {/* <AuthScreen /> */}
+      <HomeScreen />
     </NotificationProvider>
+    
   );
 }
